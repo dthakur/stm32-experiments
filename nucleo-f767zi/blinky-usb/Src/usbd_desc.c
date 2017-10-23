062ca4e5a4c2edcb6170eae9a8b9cf630b6f3ea0
Divya Thakur <rewire@gmail.com>
2017-10-22 21:19:31 -0700

    revert some names

--- nucleo-f767zi/blinky-usb/Src/usbd_desc.c
+++ nucleo-f767zi/blinky-usb/Src/usbd_desc.c
@@ -75,7 +75,7 @@
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
 #define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "Divya's STM32 Virtual ComPort"
+#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
