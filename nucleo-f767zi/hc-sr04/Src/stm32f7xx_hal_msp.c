c12914686e7a593f2eaaf745f6efa1294a50c302
Divya Thakur <rewire@gmail.com>
2018-03-18 11:58:49 -0700

    circular mode

--- nucleo-f767zi/hc-sr04/Src/stm32f7xx_hal_msp.c
+++ nucleo-f767zi/hc-sr04/Src/stm32f7xx_hal_msp.c
@@ -116,7 +116,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     hdma_tim4_ch1.Init.MemInc = DMA_MINC_ENABLE;
     hdma_tim4_ch1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_tim4_ch1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-    hdma_tim4_ch1.Init.Mode = DMA_NORMAL;
+    hdma_tim4_ch1.Init.Mode = DMA_CIRCULAR;
     hdma_tim4_ch1.Init.Priority = DMA_PRIORITY_MEDIUM;
     hdma_tim4_ch1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_tim4_ch1) != HAL_OK)
